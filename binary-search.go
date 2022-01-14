f9a201dbbe40707453eb03b2bb360c712182e936
Jônatas <jonatasjr.019@gmail.com>
2022-01-14 19:41:12 -0300

    função de busca binaria

--- binary-search.go
+++ binary-search.go
@@ -1,9 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 func main() {
-	fmt.Println(generateList(10))
+	list := generateList(10)
+	pos, err := binarySearch(list, 6)
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Println("valor encontrado na posição:", pos)
+
+}
+
+func binarySearch(list []int, value int) (int, error) {
+	inicio := 0
+	fim := len(list) - 1
+
+	for inicio <= fim {
+		meio := (inicio + fim) / 2
+		chute := list[meio]
+		switch {
+		case chute == value:
+			return meio, nil
+		case chute < value:
+			inicio = meio + 1
+		case chute > value:
+			fim = meio - 1
+		}
+	}
+	return -1, errors.New("valor não encontrado")
 }
 
 func generateList(x int) []int {
